3b2e38f49f7dd1f66278932a03442d984950b28d
Louis-David Lacerte <ldlacerte@hotmail.com>
2017-06-12 19:02:58 -0400

    First push

---
+++ Utils/Strings.cs
@@ -0,0 +1,40 @@
+﻿namespace LaLifeWrapper.Utils
+{
+    static class Strings
+    {
+        public static bool ACCESS = false;
+
+
+        /* NOTIFICATIONS */
+
+        public const string NO_TARGET = "~r~ Vous n'avez pas de cible.";
+
+        public const string PLAYER_JOINED = "{0} joined the game.";
+        public const string PLAYER_LEFT = "{0} left the game.";
+
+        /* INTERACTION MENU */
+
+        public const string INTERACTIONMENU_MAIN_TITLE = "Itinerance";
+        public const string INTERACTIONMENU_MAIN_SUBTITLE = "Interaction";
+
+        public const string INTERACTIONMENU_PLAYERSKIN_MENU = "Player Skin";
+        public const string INTERACTIONMENU_PLAYERSKIN_SUBTITLE = "Change your skin";
+        public const string INTERACTIONMENU_PLAYERSKIN_SEARCH = "Search";
+        public const string INTERACTIONMENU_PLAYERSKIN_CHANGING = "Changing Player model...";
+        public const string INTERACTIONMENU_PLAYERSKIN_INVALID = "Invalid Player model.";
+
+        /* PHONE MENU */
+
+        public const string PHONEMENU_MAIN_TITLE = "Phone";
+        public const string PHONEMENU_MAIN_SUBTITLE = "Main Menu";
+
+        public const string PHONEMENU_MISSIONS_MENU = "Missions";
+        public const string PHONEMENU_MISSIONS_MISSIONRUNNING = "A mission is already in progress.";
+        public const string PHONEMENU_MISSIONS_MISSIONSTARTED = "{0} started a new mission.";
+        public const string PHONEMENU_MISSIONS_MISSIONSTOPPED = "{0} finished a mission.";
+        public const string PHONEMENU_MISSIONS_MISSIONCOOLDOWN = "You have to wait {0} seconds until you can start another mission.";
+
+        public const string PHONEMENU_PLAYERLIST_MENU = "Players";
+
+    }
+}
